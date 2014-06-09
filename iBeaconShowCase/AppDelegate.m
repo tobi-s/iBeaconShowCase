67de307792808d591456edd587dbccd0546e07e3
tobi-s <protobi@gmail.com>
2014-06-09 14:42:11 +0200

    So far so good

--- iBeaconShowCase/AppDelegate.m
+++ iBeaconShowCase/AppDelegate.m
@@ -10,14 +10,6 @@
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
-    return YES;
-}
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {

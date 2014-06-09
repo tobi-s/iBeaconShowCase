7e0e3350c49d047cf5f61e271a6f63ebfa223d4c
tobi-s <protobi@gmail.com>
2014-06-09 13:30:19 +0200

    Initial Commit

---
+++ iBeaconShowCase/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iBeaconShowCase
+//
+//  Created by iTobi on 09/06/14.
+//  Copyright (c) 2014 Tobias Schade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iBeaconShowCaseTests/iBeaconShowCaseTests.m
@@ -0,0 +1,34 @@
+//
+//  iBeaconShowCaseTests.m
+//  iBeaconShowCaseTests
+//
+//  Created by iTobi on 09/06/14.
+//  Copyright (c) 2014 Tobias Schade. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iBeaconShowCaseTests : XCTestCase
+
+@end
+
+@implementation iBeaconShowCaseTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
